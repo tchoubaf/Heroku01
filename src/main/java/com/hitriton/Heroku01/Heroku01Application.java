aae54b120b1cd77c270c08a01ec9e05c793c03e1
tchoubaf <tchoubaf@hitriton.com>
2020-05-15 08:50:55 -0500

    my first commit

---
+++ src/main/java/com/hitriton/Heroku01/Heroku01Application.java
@@ -0,0 +1,13 @@
+package com.hitriton.Heroku01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Heroku01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Heroku01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/hitriton/Heroku01/Heroku01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hitriton.Heroku01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Heroku01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
